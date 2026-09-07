da3b641c4f4f6f222f84893ee14dee483a54272a
肖海善 <72793467+xiaohandsome@users.noreply.github.com>
2026-09-07 16:24:43 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
     from predictor import predictor as pr
     from cell_tracking import tracker as ct
     import datetime
+    import os.path.join as join
+    from hydra.utils import to_absolute_path as abs_path
 
     now = datetime.datetime.now()
     print('START!', now)
@@ -19,9 +21,9 @@ if __name__ == '__main__':
     import numpy as np
     from convert2CTCFormat import lineage_mask
 
-    mask_dir = r"data\mask"   ###  SIM+=GT
-    track_dir = r"result\track_results.csv"
-    centroid = r"result\centroid.npy"
+    mask_dir = abs_path(join('data', 'mask'))
+    track_dir = abs_path(join("result", "track_results.csv"))
+    centroid = abs_path(join("result", "centroid.npy"))
 
     tracks = pd.read_csv(track_dir).to_numpy()
     cnt = np.load(centroid, allow_pickle=True)
